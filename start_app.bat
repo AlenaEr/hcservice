7bb75816d833912c75ac1ba1232836630a48f7d2
alenaer <alana.eremina@gmail.com>
2023-12-21 13:25:07 +0100

    update start script

--- start_app.bat
+++ start_app.bat
@@ -1,5 +1,5 @@
 @echo off
-
+mvn clean install
 chmod +x target\hcservices-0.0.1-SNAPSHOT.jar
 mvn javadoc:javadoc
 if exist "target\hcservices-0.0.1-SNAPSHOT.jar" (
@@ -7,4 +7,5 @@ if exist "target\hcservices-0.0.1-SNAPSHOT.jar" (
     java -jar target\hcservices-0.0.1-SNAPSHOT.jar
 ) else (
     echo Error: JAR file not found.
+    mvn spring-boot:run
 )
--- start_app.sh
+++ start_app.sh
@@ -1,11 +1,12 @@
 #!/bin/bash
 
 chmod +x target/hcservices-0.0.1-SNAPSHOT.jar
-# Генерація JavaDoc
+mvn clean install
 mvn javadoc:javadoc
 if [ -e "target/hcservices-0.0.1-SNAPSHOT.jar" ]; then
     echo "Run application: target/hcservices-0.0.1-SNAPSHOT.jar"
     java -jar target/hcservices-0.0.1-SNAPSHOT.jar
 else
-    echo "Exception: JAR-file not found."
+    echo "Exception: JAR-file not found. Trying to run the application using Spring Boot Maven Plugin."
+    mvn spring-boot:run
 fi
